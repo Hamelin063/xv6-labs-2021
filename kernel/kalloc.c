9e0a1514d7cf40120e8e705cbc2f5ef1b0b47f49
Ha melin <hamelin@HamelinMacBook-Pro.local>
2025-05-10 19:18:01 +0800

    cow lab finished

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,10 @@
 #include "riscv.h"
 #include "defs.h"
 
+#define PA2INDEX(pa) (((uint64)pa)/PGSIZE)//索引，用来标识这个页面的被引用的次数
+
+int cowcount[PHYSTOP/PGSIZE];
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -35,8 +39,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    cowcount[PA2INDEX(p)]=1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -50,6 +56,14 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+
+  acquire(&kmem.lock);
+  int remain=--cowcount[PA2INDEX(pa)];
+  release(&kmem.lock);
+
+  if(remain>0){
+    return;//还有引用，不能free
+  }
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
@@ -76,7 +90,22 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
+  if(r) {
+    memset((char *)r, 5, PGSIZE); // fill with junk
+    int idx = PA2INDEX(r);
+    if (cowcount[idx] != 0) {
+      panic("kalloc: cowcount[idx] != 0");
+    }
+    cowcount[idx] = 1; // 新allocate的物理页的计数器为1
+  }
   return (void*)r;
+}
+
+void refcountplus(uint64 pa,int num){
+  if(pa>=PHYSTOP){
+    panic("refcountplus: pa too big");
+  }
+  acquire(&kmem.lock);
+  cowcount[PA2INDEX(pa)]+=num;
+  release(&kmem.lock);
 }
--- kernel/vm.c
+++ kernel/vm.c
@@ -303,7 +303,6 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -311,14 +310,16 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
+    *pte &= ~PTE_W;//设置成只读页面
+    *pte |= PTE_COW;
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+/*    if((mem = kalloc()) == 0)
+      goto err; // 不再分配新的页面，而是直接映射
+    memmove(mem, (char*)pa, PGSIZE);*/
+    if(mappages(new, i, PGSIZE, pa, flags) != 0){
       goto err;
     }
+    refcountplus(pa,1);
   }
   return 0;
 
@@ -350,7 +351,21 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
-    pa0 = walkaddr(pagetable, va0);
+    if (va0 >= MAXVA) {
+      printf("copyout: va exceeds MAXVA\n");
+      return -1;
+    }
+    pte_t *pte=walk(pagetable,va0,0);
+    if (pte == 0 || (*pte & PTE_U) == 0 || (*pte & PTE_V) == 0){
+      printf("copyout: invalid pte\n");
+      return -1;
+    }
+    if((*pte & PTE_COW)){
+      if(cowalloc(pagetable,va0)<0){
+        return -1;
+      }
+    }
+    pa0 = PTE2PA(*pte);
     if(pa0 == 0)
       return -1;
     n = PGSIZE - (dstva - va0);
@@ -431,4 +446,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
+}
+
+int cowalloc(pagetable_t pagetable, uint64 va){
+  if(va>=MAXVA){
+    printf("cowalloc: va exceeded");
+    return -1;
+  }
+  pte_t* pte=walk(pagetable,va,0);
+  if(pte==0){
+    panic("cowalloc: pte not exists");
+  }
+  if((*pte & PTE_V)==0||(*pte & PTE_U)==0||(*pte & PTE_COW)==0){
+    panic("cowalloc: pte permission denied");
+  }
+  uint64 pa_old=PTE2PA(*pte);
+  uint64 pa_new=(uint64)kalloc();
+  if(pa_new==0){
+    printf("cowalloc: kalloc failed");
+    return -1;
+  }
+  memmove((void *)pa_new,(const void *)pa_old,PGSIZE);
+  kfree((void *)pa_old);
+  *pte=PA2PTE(pa_new) | PTE_FLAGS(*pte) | PTE_W;//更新页表项
+  *pte &= ~PTE_COW;//清除cow位
+  return 0;
 }
