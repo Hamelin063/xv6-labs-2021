3115047786a2c9f709c458e6f07b73ac5333ddb8
Ha melin <hamelin@HamelinMacBook-Pro.local>
2025-05-20 19:17:18 +0800

    thread lab finished

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,15 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t mutex[NBUCKET];
+
+
+void init_locks() {
+  // 在main函数一开始 呼叫一下这个函数
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&mutex[i], NULL);
+  }
+}
 
 double
 now()
@@ -41,6 +50,7 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&mutex[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -54,7 +64,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&mutex[i]);
 }
 
 static struct entry*
@@ -62,12 +72,12 @@ get(int key)
 {
   int i = key % NBUCKET;
 
-
+  pthread_mutex_lock(&mutex[i]);
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
+  pthread_mutex_unlock(&mutex[i]);
   return e;
 }
 
@@ -105,6 +115,7 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
+  init_locks();
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
